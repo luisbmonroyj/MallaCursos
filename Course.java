2ed25deec3f040a3e2c2b94e8a0dbe46f692ea6d
Luis Bernardo Monroy Jaramillo <111471014+luisbmonroyj@users.noreply.github.com>
2025-01-13 14:44:48 -0500

    Update Course.java
    class complete

--- Course.java
+++ Course.java
@@ -15,7 +15,54 @@ public class Course {
   }  
 
   public Course(String name,String description,int duration,double price,TimeStamp beginningDate,BigInteger idTeacher){
+    this.name = name;
+    this.description = description;
+    this.duration = duration; 
+    this.price = price;
+    this.beginningDate = beginningDate;
+    this.idTeacher= idTeacher;
   }  
 
+  @getters
+  public String getName (){
+    return this.name; 
+  }
+  public String getDescription (){
+    return this.name; 
+  }
+  public int getDuration (){
+    return this.duration; 
+  }
+  public double getPrice (){
+    return this.price; 
+  }
+  public TimeStamp getBeginningDate (){
+    return this.beginningDate; 
+  }
+  public BigInteger getIdTeacher (){
+    return this.idTeacher; 
+  }
 
+  @setters
+  public void setName(String name){
+    this.name = name;
+  }
+  public void setDescription (String description){
+    
+    this.description = description;
+    
+  }
+  public void setDuration (int duration){
+    this.duration = duration; 
+    
+  }
+  public void setPrice (double price){
+    this.price = price;
+  }
+  public void setBeginningDate (TimeStamp beginningDate){
+    this.beginningDate = beginningDate;
+  }
+  public void setIDTeacher (BigInteger idTeacher){
+    this.idTeacher= idTeacher;
+  }
 }
