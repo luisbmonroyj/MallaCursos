5914849b077c4ceeb1f2c635d40ee929261384b8
Luis Bernardo Monroy Jaramillo <111471014+luisbmonroyj@users.noreply.github.com>
2025-01-13 15:50:05 -0500

    Update Administration.java
    methods for creating and reading courses and teachers

--- Administration.java
+++ Administration.java
@@ -14,18 +14,19 @@ public class Administration {
        // Register the PostgreSQL driver
       
     }
-
-       public static void insertTeacher(Teacher teacher){
+    
+@create
+public static void insertTeacher(Teacher teacher){
     String insertString = "INSERT INTO teacher (id,name,lastname,email) VALUES ("+teacher.toInsertValues()+")";
   insertValues (insertString,false);
   }
 
-    public static void insertCourse(Course curso){
+public static void insertCourse(Course curso){
     String insertString = "INSERT INTO course (name,description,duration,price,beginningDate,idTeacher) VALUES ("+curso.toInsertValues()+")";
   insertValues (insertString,false);
   }
 
-  public static void insertValues (String query, boolean echo){
+public static void insertValues (String query, boolean echo){
     Class.forName("org.postgresql.Driver");
     try(Connection connection = DriverManager.getConnection(jdbcUrl, username, password){
       Statement statement = connection.createStatement();
@@ -39,6 +40,74 @@ public class Administration {
     connection.close();
     }
   }
+@read
+public static Teacher[] teachers (String where) {
+        int counter = 0;
+        Teacher[] profesores = new Teacher[getRowCount("teacher "+where,false)];
+        // create a database connection
+        try (Connection connection = DriverManager.getConnection(url);){
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM teacher "+where);
+            while(rs.next()) {
+                BigInteger id = rs.getInt("id");
+                String name = rs.getString("name");
+                String lastname = rs.getString("lastname");
+                String email = rs.getString("email");
+                profesores[counter] = new Course(name,description,duration,price,beginningDate,idTeacher);  
+                counter++;
+            }
+            sqliteConnection.close();
+        }
+        catch(SQLException e)
+            {e.printStackTrace(System.err); }
+     
+    return profesores;
+    }
+
+public static course[] courses (String where) {
+        int counter = 0;
+        Course[] cursos = new Course[getRowCount("course "+where,false)];
+        // create a database connection
+        try (Connection connection = DriverManager.getConnection(url);){
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM course "+where);
+            while(rs.next()) {
+                String name = rs.getString("name");
+                String description = rs.getString("description");
+                int duration = rs.getInt("duration"); 
+                double price = rs.getDouble("price");
+                TimeStamp beginningDate = rs.getTimeStamp("beginningDate");
+                BigInteger idTeacher = rs.getInt("idTeacher);
+                cursos[counter] = new Course(name,description,duration,price,beginningDate,idTeacher);  
+                counter++;
+            }
+            sqliteConnection.close();
+        }
+        catch(SQLException e)
+            {e.printStackTrace(System.err); }
+     
+    return cursos;
+    }
+
+public static int getRowCount (String table, boolean orderByDate) {
+        //this method helps create the object array in perfect length
+        String queryString = "SELECT * FROM "+table;
+        if (orderByDate)
+            queryString += " ORDER BY date";
+        // create a database connection
+        try (Connection sqliteConnection = DriverManager.getConnection(url);){
+            Statement statement = sqliteConnection.createStatement();
+            ResultSet rs = statement.executeQuery(queryString);
+            while(rs.next())
+                counter++;
+            sqliteConnection.close();
+        }
+        catch(SQLException e)
+            {e.printStackTrace(System.err); }
+        
+    return counter;
+    }
+
   
     
 
