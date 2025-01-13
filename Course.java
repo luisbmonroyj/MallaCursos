b9152db50c990d2c0e5014a2602cb3308b507389
Luis Bernardo Monroy Jaramillo <111471014+luisbmonroyj@users.noreply.github.com>
2025-01-13 14:35:39 -0500

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,21 @@
+import java.sql.Timestamp;
+
+public class Course {
+
+  private String name;
+  private String description;
+  private int duration;
+  private double price;
+  private TimeStamp beginningDate;
+  private BigInteger idTeacher;
+
+  
+  public Course(){
+    
+  }  
+
+  public Course(String name,String description,int duration,double price,TimeStamp beginningDate,BigInteger idTeacher){
+  }  
+
+
+}
