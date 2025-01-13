b290304fa9918c37df1d7bb39f50e94aba44e9b1
Luis Bernardo Monroy Jaramillo <111471014+luisbmonroyj@users.noreply.github.com>
2025-01-13 14:53:21 -0500

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS "course";
+DROP TABLE IF EXISTS "teacher";
+
+CREATE TABLE "teacher" (
+  "id" BIGINT NOT NULL,
+  "name" TEXT NOT NULL,
+  "lastname" TEXT NOT NULL,
+  "email" TEXT NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "course" (
+  "name" TEXT NOT NULL,
+  "description" TEXT NOT NULL,
+  "duration" INTEGER NOT NULL,
+  "price" DOUBLE NOT NULL DEFAULT 0.0,
+  "beginningDate" TIMESTAMP NOT NULL,
+  "idTeacher" BIGINT NOT NULL,
+  FOREIGN KEY ("idTeacher") REFERENCES "teacher"."id"
+);
+
